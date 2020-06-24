6e65a37fdd40a190795ed98a3725ba77b4388bfa
piupaulgit <piupaul99design@gmail.com>
2020-06-25 01:59:33 +0530

    demo page started

--- projects/ngc-progressbar-example/src/app/app.module.ts
+++ projects/ngc-progressbar-example/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgcProgressbarModule } from 'projects/ngc-progressbar/src/public-api';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SidenavComponent],
   imports: [BrowserModule, NgcProgressbarModule],
   providers: [],
   bootstrap: [AppComponent],
